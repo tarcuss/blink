af5e4c0257c336206ee2f806d1e16e7d77124616
tarcuss <103863429+tarcuss@users.noreply.github.com>
2024-09-25 13:26:41 +0300

    Delete build directory

--- build/image_preload.cmake
+++
@@ -1,6 +0,0 @@
-# Generated file that can be used to preload variant images
-set(BOARD "nrf52dk/nrf52832" CACHE INTERNAL "NCS child image controlled")
-set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
-set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON "/opt/nordic/ncs/toolchains/f8037e9b83/opt/python@3.9/bin/python3.9" CACHE INTERNAL "NCS child image controlled")
